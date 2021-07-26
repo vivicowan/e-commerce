49ab9944e5e1c0f8ebd99790c7d928fdaa178687
Vivi Cowan <vivicare@gmail.com>
2021-07-25 19:23:37 -0700

    post route for tags created

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -37,12 +37,11 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try {
-    const travellerData = await Traveller.create(req.body);
-    res.status(200).json(travellerData);
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
   }
-
 });
 
 router.put('/:id', (req, res) => {
