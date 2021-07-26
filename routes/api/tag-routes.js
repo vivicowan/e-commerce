7de4579f4792f6c5a44e6d54bd035eb26b16c52b
Vivi Cowan <vivicare@gmail.com>
2021-07-25 19:20:37 -0700

    find single tag by its id, completed

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -18,7 +18,20 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  try {
+    const tagData = await Product.findByPk(req.params.id, {
+      // be sure to include its associated Product data
+      include: [{ model: Product, through: ProductTag }],
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 router.post('/', (req, res) => {
