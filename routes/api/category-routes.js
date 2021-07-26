a3264914b61b77ee4916b939fb143c992c201d23
Vivi Cowan <vivicare@gmail.com>
2021-07-25 20:07:52 -0700

    delete route created

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -65,6 +65,20 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
