d2cd5c943cb04af2b92c662eda2b57d305888cfe
Vivi Cowan <vivicare@gmail.com>
2021-07-25 20:00:24 -0700

    created update tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -46,6 +46,20 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!tagData[0]) {
+      res.status(404).json({ message: "No tag found with that id!" });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
