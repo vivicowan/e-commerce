0251b5d431e30e3844eaa8c5f962833feb3a16d3
Vivi Cowan <vivicare@gmail.com>
2021-07-25 19:17:45 -0700

    created a get all tags route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,7 +5,15 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      // be sure to include its associated Product data
+      include: [{ model: Product, through: ProductTag }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
