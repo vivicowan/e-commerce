261c5846777123f03f87fe3eb1fd91fe3f411510
Vivi Cowan <vivicare@gmail.com>
2021-07-25 20:59:17 -0700

    fixed the mixed model names

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   try {
-    const tagData = await Product.findByPk(req.params.id, {
+    const tagData = await Tag.findByPk(req.params.id, {
       // be sure to include its associated Product data
       include: [{ model: Product, through: ProductTag }],
     });
